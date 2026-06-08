2d904ba7dee4730a7cc9266e2d474fd89c2e948b
Naveenjavvadi21 <naveenjavvadi21@gmail.com>
2026-06-08 15:48:56 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+function showMessage()
+{
+    alert("Thank you for visiting my portfolio!");
+}
+
+function downloadResume()
+{
+    window.open(
+        "https://drive.google.com/file/d/1mYPuWOFZUabFU6RmBmOW_l1r18jjzXq-/view?usp=sharing",
+        "_blank"
+    );
+}
